5c03b8cb605db942f869d26e1dd7ea827389b3ab
Ankit Ydv <ankityadav11241@gmail.com>
2024-08-10 22:14:06 +0530

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import { Navbar } from "./components/Navbar";
 import { RouteNotFound } from "./routes/RouteNotFound";
+import Dashboard from "./routes/Dashboard";
+import Products from "./routes/Products";
+import { ThemeProvider } from "./hooks/context/theme-provider";
 
-const router = createBrowserRouter([
-    {
-        path: "",
-        element: <Navbar />,
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-        ],
-        errorElement: <RouteNotFound />,
-    },
-]);
 export default function App() {
-    return (
-        <div>
-            {/* <Navbar /> */}
-            <RouterProvider router={router} />
-        </div>
-    );
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="overflow-hidden h-screen bg-light-background text-light-text dark:bg-dark-background dark:text-dark-text">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/">
+              <Route path="/" element={<Home />} />
+              <Route path="products" element={<Products />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+            <Route path="*" element={<RouteNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,41 @@
 import { NavLink } from "react-router-dom";
+import { ThemeToggle } from "./ThemeToggle";
 
 const navItems = [
-    {
-        name: "Home",
-        href: "/",
-    },
-    {
-        name: "Products",
-        href: "/products",
-    },
-    {
-        name: "Orders",
-        href: "/orders",
-    },
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Products",
+    href: "/products",
+  },
+  {
+    name: "Orders",
+    href: "/orders",
+  },
 ];
 
 export function Navbar() {
-    return (
-        <nav className="shadow-sm shadow-yellow-400 p-2 w-full">
-            <div className="md:w-2/5 flex justify-between mx-auto">
-                <div>Logo</div>
-                <ul className="flex md:gap-8 text-sm">
-                    {navItems.map((item, idx) => {
-                        return (
-                            <NavLink to={item.href} key={idx}>
-                                <li>{item.name}</li>
-                            </NavLink>
-                        );
-                    })}
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="shadow-sm shadow-slate-500 p-2 w-full">
+      <div className="md:w-3/5 flex justify-between mx-auto">
+        <div>Logo</div>
+        <div>
+          <ul className="flex md:gap-8 text-sm">
+            {navItems.map((item, idx) => {
+              return (
+                <li className="px-2 py-1" key={idx}>
+                  <NavLink to={item.href} className={({ isActive }) => (isActive ? "bg-green-400 rounded-md text-zinc-700 p-2" : "p-2")}>
+                    {item.name}
+                  </NavLink>
+                </li>
+              );
+            })}
+            <ThemeToggle />
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
