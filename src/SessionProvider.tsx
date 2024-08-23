04904842b2ead25891c7369eb4b5fd78e1081445
Ankit Ydv <ankityadav11241@gmail.com>
2024-08-24 03:46:34 +0530

    fix: session provider

--- src/SessionProvider.tsx
+++ src/SessionProvider.tsx
@@ -1,18 +1,22 @@
 import { useEffect } from "react";
 import { useAppDispatch } from "./hooks/store";
 import axios from "axios";
+import { login } from "./lib/store/features/authSlice";
 
 export default function SessionProvider(): null {
   const dispatch = useAppDispatch();
   useEffect(() => {
     (() => {
+      console.log("logging");
+
       axios({
         method: "GET",
         url: `${import.meta.env.VITE_SERVER_URL}/api/v1/auth/refresh`,
         withCredentials: true,
       })
         .then((res) => {
-          dispatch({ type: "auth", payload: res?.data?.data });
+          const user = res.data.data;
+          dispatch(login({ id: user.id, email: user.email, name: user.name }));
           return;
         })
         .catch(() => {
